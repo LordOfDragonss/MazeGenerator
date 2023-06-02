cd8e23a5f2e3a4716d69d94882eb07dd61bf6597
MikeTool <miketooltje@gmail.com>
2023-06-02 17:24:24 +0200

    fixed victorysequence

--- Maze/Assets/Scripts/VictorySequence.cs
+++ Maze/Assets/Scripts/VictorySequence.cs
@@ -9,19 +9,20 @@ public class VictorySequence : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(tracker.score == tracker.maxScore && tracker.score != 0)
-        {
-            victoryScreen.SetActive(true);
-        }
-        else
-        {
-            victoryScreen.SetActive(false);
-        }
+        if (tracker.score != 0)
+            if (tracker.score == tracker.maxScore)
+            {
+                victoryScreen.SetActive(true);
+            }
+            else
+            {
+                victoryScreen.SetActive(false);
+            }
     }
 }
