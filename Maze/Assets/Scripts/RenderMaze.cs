19ffa195cf58bb4410bf0dff92fc2c65c449a81b
MikeTool <miketooltje@gmail.com>
2023-10-31 23:33:31 +0100

    changed the rendering so that it doesn't have seperate functions per generator instead does if the maze is colored else it'll still do the same

--- Maze/Assets/Scripts/RenderMaze.cs
+++ Maze/Assets/Scripts/RenderMaze.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 public class RenderMaze : MonoBehaviour
 {
     [SerializeField] public MazeGenerator mazeGenerator;
-    MazeGeneratorColor colorMaze;
     [SerializeField] GameObject cellPrefabObject;
     Cell[,] maze;
     public CellObject[,] cells;
 
     public float cellSize = 1f;
 
+
     private void Start()
     {
-        if (mazeGenerator.GetType() == typeof(MazeGenerator))
-            GenerateMaze();
-        if (mazeGenerator.GetType() == typeof(MazeGeneratorColor))
-        {
-            colorMaze = (MazeGeneratorColor)mazeGenerator;
-            GenerateColorMaze();
-        }
+        GenerateMaze();
     }
+
     public void GenerateMaze()
     {
         maze = mazeGenerator.GetMaze();
@@ -38,34 +33,10 @@ public class RenderMaze : MonoBehaviour
                 //get the cellObject component to use it in further code
                 CellObject cell = newCell.GetComponent<CellObject>();
 
-                //Check the walls to display
-                bool top = maze[x, y].topWall;
-                bool left = maze[x, y].leftWall;
-
-                bool right = false;
-                bool bottom = false;
-                if (x == mazeGenerator.mazeWidth - 1) right = true;
-                if (y == 0) bottom = true;
-                cell.Init(top, bottom, left, right);
-                cells[x, y] = cell;
-            }
-        }
-    }
-    public void GenerateColorMaze()
-    {
-        maze = mazeGenerator.GetMaze();
-        cells = new CellObject[mazeGenerator.mazeWidth, mazeGenerator.mazeHeight];
-        for (int x = 0; x < mazeGenerator.mazeWidth; x++)
-        {
-            for (int y = 0; y < mazeGenerator.mazeHeight; y++)
-            {
-                //Instantiate a new cell based on given position and size
-                GameObject newCell = Instantiate(cellPrefabObject, new Vector3((float)x * cellSize, 0f, (float)y * cellSize), Quaternion.identity, transform);
-                //Scale the genereted cell based on cellSize
-                newCell.gameObject.transform.localScale = new Vector3(newCell.gameObject.transform.localScale.x * cellSize, newCell.gameObject.transform.localScale.y * cellSize, newCell.gameObject.transform.localScale.z * cellSize); // code to scale the cells based on cellsize
-                //get the cellObject component to use it in further code
-                CellObject cell = newCell.GetComponent<CellObject>();
-                colorMaze.GenerateRandomColoredWalls(cell);
+                if (mazeGenerator is MazeGeneratorColor)
+                {
+                    ((MazeGeneratorColor)mazeGenerator).GenerateRandomColoredWalls(cell);
+                }
 
                 //Check the walls to display
                 bool top = maze[x, y].topWall;
